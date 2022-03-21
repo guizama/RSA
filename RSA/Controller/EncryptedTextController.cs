d364feda4852ec018c870a7168bcb0bef1a5d1a6
martinelli70 <guilhermezanarellimartinelli@hotmail.com>
2022-03-20 23:07:18 -0300

    included api routes

--- RSA/Controller/EncryptedTextController.cs
+++ RSA/Controller/EncryptedTextController.cs
@@ -1,4 +1,5 @@
-﻿using RSA.Domain;
+﻿using Microsoft.AspNetCore.Mvc;
+using RSA.Domain;
 using RSA.Services.Interface;
 
 namespace RSA.Controller
@@ -12,11 +13,13 @@ namespace RSA.Controller
             this.ser = ser;
         }
 
+        [Route("/v1/text-management")]
         public InsertReturn TextManagement(InsertRequest request)
         {
             return ser.InsertText(request);
         }
 
+        [Route("/v1/text-management?id={text_id}")]
         public SelectReturn TextManagement(int id)
         {
             return ser.SelectText(id);
